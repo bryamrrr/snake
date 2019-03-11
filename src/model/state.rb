803c279540150483a01e502856a1601368c7f809
Bryam Rodriguez <brodriguezaybar@gmail.com>
2019-03-10 18:42:56 -0600

    Add basic folders

---
+++ src/model/state.rb
@@ -0,0 +1,17 @@
+# State of the game
+module Model
+  class Coord < Struct.new(row, col)
+  end
+
+  class Food < Coord
+  end
+
+  class Snake < Struct.new(positions)
+  end
+
+  class Grid < Struct.new(rows, cols)
+  end
+
+  class State < Struct.new(snake, food, grid)
+  end
+end
